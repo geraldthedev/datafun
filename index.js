e8ae292ecf9829abd18a2a89def687014dc405f6
Gerald Bryant <hakeem1106@gmail.com>
2019-01-27 16:47:25 +0000

    more set up

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://gbryant:Blessedautumn1106@cluster0-shard-00-00-xutke.gcp.mongodb.net:27017,cluster0-shard-00-01-xutke.gcp.mongodb.net:27017,cluster0-shard-00-02-xutke.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', {useNewUrlParser:true});
-
-const connection = mongoose.connection;
-
-connection.then((db) =>{
-    console.log('connection....nice');
-
-}).catch((err)=> {
-    console.log('fix it', err)
-
-});
+mongoose.connect('mongodb://gbryant:'+ process.env.MONGO_PASS + '@cluster0-shard-00-00-xutke.gcp.mongodb.net:27017,cluster0-shard-00-01-xutke.gcp.mongodb.net:27017,cluster0-shard-00-02-xutke.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', {useNewUrlParser:true});
